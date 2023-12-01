2dee7ea9ceac61b9e11a7e17db8523aac8cb9f8d
Joost van Wollingen <joostvanwollingen@gmail.com>
2023-12-01 21:53:33 +0100

    AOC 2023 Day 1

---
+++ src/main/kotlin/day1.kt
@@ -0,0 +1,37 @@
+import java.io.File
+
+fun main() {
+    val lines = File("day1.real.txt").readLines()
+    val result = lines.sumOf { "${getFirstNumber(it)}${getLastNumber(it)}".toInt() }
+    println(result)
+}
+
+fun getNumbers(input: String): Sequence<MatchResult> {
+    val regex = Regex("([0-9]{1})|(?=(one|two|three|four|five|six|seven|eight|nine)).")
+    return regex.findAll(input)
+}
+
+fun getFirstNumber(input: String): String {
+    val matches = getNumbers(input)
+    return toNumberString(matches.first().groupValues.findLast { it.isNotEmpty() }!!)
+}
+
+fun getLastNumber(input: String): String {
+    val matches = getNumbers(input)
+    return toNumberString(matches.last().groupValues.findLast { it.isNotEmpty() }!!)
+}
+
+private fun toNumberString(input: String): String {
+    return when (input) {
+        "one" -> "1"
+        "two" -> "2"
+        "three" -> "3"
+        "four" -> "4"
+        "five" -> "5"
+        "six" -> "6"
+        "seven" -> "7"
+        "eight" -> "8"
+        "nine" -> "9"
+        else -> input
+    }
+}
