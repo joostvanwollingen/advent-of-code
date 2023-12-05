61534e2c91d4a7038d4a745f77bdaffa4cf2cc77
Joost van Wollingen <joostvanwollingen@gmail.com>
2023-12-05 12:53:59 +0100

    Day 5 part 2, test ok

--- src/main/kotlin/day5.kt
+++ src/main/kotlin/day5.kt
@@ -1,33 +1,54 @@
 fun main() {
-    val input = AocUtil.load("day5.input")
-    val testInput = AocUtil.load("day5.test.input")
-    solvePartOne(testInput)
-    solvePartTwo()
+//    val input = AocUtil.load("day5.input")
+    val input = AocUtil.load("day5.test.input")
+//    solvePartOne(input)
+    solvePartTwo(input)
 }
 
 fun solvePartOne(input: String) {
-    val seeds = input.lines().get(0).split(":")[1].trim().split(" ").map { seed -> seed.toInt() }
-    val conversionMaps = input.reader().readText().drop(20).split("\n\n").map { line -> line.split(":") }
+    val line1Length = input.lines().get(0).length
+    val seeds = input.lines().get(0).split(":")[1].trim().split(" ").map { seed -> seed.toLong() }
+    val conversionMaps = input.reader().readText().drop(line1Length).split("\n\n").map { line -> line.split(":") }
         .map { line -> line.first() to line.last().split("\n").filter { line -> line.isNotEmpty() } }.map { line ->
             line.first to line.second.map { r -> NumberRange(r) }
         }.map { m -> ConversionMap(m.first, m.second) }
 
+    val seedLocations: MutableList<Pair<Long, Long>> = mutableListOf()
     for (seed in seeds) {
+        println("Converting seed $seed")
         var sourceNumber = seed
-        var destination: Int? = null
+        var destination: Long? = null
         for (map in conversionMaps) {
             destination = map.convert(sourceNumber)
             sourceNumber = destination
+            println("$sourceNumber - ${map.name} - $destination")
+
         }
-        println("$seed $destination")
+        seedLocations.add(seed to destination!!)
     }
+    println(seedLocations)
+    println("Lowest ${seedLocations.minBy { i -> i.second }}")
 
 }
 
-fun solvePartTwo() {}
+fun solvePartTwo(input: String) {
+    val seedRanges = input.lines().get(0).split(":")[1].trim().split(" ")
+    val seeds = mutableListOf<Long>()
+    seedRanges.forEachIndexed { index, s ->
+        if (index % 2 == 0) {
+            var start = s.toLong()
+            var end = s.toLong() + seedRanges[index+1].toLong() -1
+            for (b in start..end) {
+                seeds.add(b)
+            }
+        }
+    }
+    println(seeds.sortedBy { it })
+    println(seeds.size)
+}
 
 data class ConversionMap(val name: String, val conversionRanges: List<NumberRange>) {
-    fun convert(sourceNumber: Int): Int {
+    fun convert(sourceNumber: Long): Long {
         val conversions = conversionRanges.map { range ->
             range.convert(sourceNumber)
         }.filter { c -> c != sourceNumber }
@@ -37,15 +58,15 @@ data class ConversionMap(val name: String, val conversionRanges: List<NumberRang
 
 data class NumberRange(val input: String) {
     private val splitInput = input.split(" ")
-    val destinationRangeStart = splitInput[0].toInt()
-    val sourceRangeStart: Int = splitInput[1].toInt()
-    val rangeLength: Int = splitInput[2].toInt()
-    fun convert(sourceNumber: Int): Int {
-        var sourceRange = sourceRangeStart..sourceRangeStart + rangeLength
-        var destinationRange = destinationRangeStart..destinationRangeStart + rangeLength
-        if (sourceRange.indexOf(sourceNumber) != -1) {
-            return destinationRange.elementAt(sourceRange.indexOf(sourceNumber))
-        }
-        return sourceNumber
+    val destinationRangeStart = splitInput[0].toLong()
+    val sourceRangeStart = splitInput[1].toLong()
+    val rangeLength = splitInput[2].toLong()
+    val sourceRangeEnd = sourceRangeStart + rangeLength
+    var sourceRange = sourceRangeStart..sourceRangeStart + rangeLength
+    var destinationRange = destinationRangeStart..destinationRangeStart + rangeLength
+    fun convert(sourceNumber: Long): Long {
+        if (sourceNumber < sourceRangeStart) return sourceNumber
+        if (sourceNumber > sourceRangeEnd) return sourceNumber
+        return destinationRange.elementAt(sourceRange.indexOf(sourceNumber))
     }
 }
