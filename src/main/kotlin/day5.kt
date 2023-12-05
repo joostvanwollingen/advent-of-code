80839989f2fe829d7af7b305c4353a713b368390
Joost van Wollingen <joostvanwollingen@gmail.com>
2023-12-05 15:39:51 +0100

    Day 5 part 2 - working

--- src/main/kotlin/day5.kt
+++ src/main/kotlin/day5.kt
@@ -6,12 +6,8 @@ fun main() {
 }
 
 fun solvePartOne(input: String) {
-    val line1Length = input.lines().get(0).length
     val seeds = input.lines().get(0).split(":")[1].trim().split(" ").map { seed -> seed.toLong() }
-    val conversionMaps = input.reader().readText().drop(line1Length).split("\n\n").map { line -> line.split(":") }
-        .map { line -> line.first() to line.last().split("\n").filter { line -> line.isNotEmpty() } }.map { line ->
-            line.first to line.second.map { r -> NumberRange(r) }
-        }.map { m -> ConversionMap(m.first, m.second) }
+    val conversionMaps = getConversionMaps(input)
 
     val seedLocations: MutableList<Pair<Long, Long>> = mutableListOf()
     for (seed in seeds) {
@@ -31,21 +27,57 @@ fun solvePartOne(input: String) {
 
 }
 
+
 fun solvePartTwo(input: String) {
-    val seedRanges = input.lines().get(0).split(":")[1].trim().split(" ")
-    val seeds = mutableListOf<Long>()
-    seedRanges.forEachIndexed { index, s ->
+    val seedRangeStartEnd = input.lines().get(0).split(":")[1].trim().split(" ")
+    val seedRanges: MutableList<Pair<Long, Long>> = mutableListOf()
+    seedRangeStartEnd.forEachIndexed { index, s ->
         if (index % 2 == 0) {
             var start = s.toLong()
-            var end = s.toLong() + seedRanges[index+1].toLong() -1
-            for (b in start..end) {
-                seeds.add(b)
-            }
+            var end = s.toLong() + seedRangeStartEnd[index + 1].toLong() - 1
+            seedRanges.add(start to end)
         }
     }
-    println(seeds.sortedBy { it })
-    println(seeds.size)
+
+    val conversionMaps = getConversionMaps(input)
+    val seeds: MutableMap<Long, Long> = mutableMapOf()
+    val finalSeeds: MutableMap<Long, Long> = mutableMapOf()
+    for (seedRange in seedRanges) {
+        println("Working on seedRange $seedRange")
+        for (i in 0..6) {
+            println(conversionMaps[i].name + " start")
+            if (i == 0) {
+                val currentSeedRange = (seedRange.first..seedRange.second)
+                println("Converting seedRange $seedRange to seeds")
+                val canConvert = conversionMaps[i].canConvert(
+                    seedRange.first, seedRange.second
+                )
+                if (!canConvert) {
+                    println("$seedRange can't be converted by ${conversionMaps[i].name}")
+                    currentSeedRange.map { seed -> seeds.put(seed, seed) }
+                } else {
+                    println("$seedRange can be converted by ${conversionMaps[i].name}")
+                    currentSeedRange.map { seed -> seeds.put(seed, conversionMaps[i].convert(seed)) }
+                }
+            } else {
+                seeds.forEach { seed ->
+                    seeds[seed.key] = conversionMaps[i].convert(seed.value)
+                }
+            }
+            println(seeds)
+            println(conversionMaps[i].name + " end")
+        }
+        println("$seedRange fully converted")
+        println(seeds)
+        println(seeds.size)
+        finalSeeds.putAll(seeds)
+        seeds.clear()
+    }
+    println(finalSeeds)
+    println(finalSeeds.size)
+    println(finalSeeds.minBy { it.value })
 }
+
 
 data class ConversionMap(val name: String, val conversionRanges: List<NumberRange>) {
     fun convert(sourceNumber: Long): Long {
@@ -53,6 +85,10 @@ data class ConversionMap(val name: String, val conversionRanges: List<NumberRang
             range.convert(sourceNumber)
         }.filter { c -> c != sourceNumber }
         return if (conversions.isNotEmpty()) conversions.first()!! else sourceNumber
+    }
+
+    fun canConvert(sourceStart: Long, sourceEnd: Long): Boolean = conversionRanges.any { map ->
+        sourceStart >= map.sourceRangeStart && sourceEnd <= map.sourceRangeEnd
     }
 }
 
@@ -69,4 +105,14 @@ data class NumberRange(val input: String) {
         if (sourceNumber > sourceRangeEnd) return sourceNumber
         return destinationRange.elementAt(sourceRange.indexOf(sourceNumber))
     }
+}
+
+private fun getConversionMaps(input: String): List<ConversionMap> {
+    val line1Length = input.lines().get(0).length
+
+    return input.reader().readText().drop(line1Length).removePrefix("\n\n").split("\n\n")
+        .map { line -> line.split(":") }
+        .map { line -> line.first() to line.last().split("\n").filter { line -> line.isNotEmpty() } }.map { line ->
+            line.first to line.second.map { r -> NumberRange(r) }
+        }.map { m -> ConversionMap(m.first, m.second) }
 }
