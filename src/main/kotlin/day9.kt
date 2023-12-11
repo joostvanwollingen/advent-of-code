75eeb142051007c60a74cfaddbc393b1be1a890f
Joost van Wollingen <joostvanwollingen@gmail.com>
2023-12-11 21:36:11 +0100

    Day 9 Part 1 OK

---
+++ src/main/kotlin/day9.kt
@@ -0,0 +1,55 @@
+fun main() {
+//    val input = AocUtil.parse("day9.test.input", History::fromString).toList()
+    val input = AocUtil.parse("day9.input", History::fromString).toList()
+//    println(input[1])
+//    println(input[0].extraPolate())
+//    println(input[1])
+//    println(input[1].extraPolate())
+//    input[1].history.forEach( ::println )
+//    println(input[1].extraPolate())
+//    println(input[2].extraPolate())
+    println(input.map { it.extraPolate() }.sum())
+}
+
+class History(val values: List<Long>) {
+    val history
+        get() = getFullHistory()
+
+    companion object {
+        fun fromString(input: String): History {
+            return History(input.split(" ").map { it.toLong() })
+        }
+    }
+
+    override fun toString(): String {
+        return "History(values=$values)"
+    }
+
+    fun getNext(): History {
+        val res: MutableList<Long> = values.foldIndexed(mutableListOf()) { i, r, v ->
+            if (i != 0) {
+                r += values[i] - values[i - 1]
+            }
+            r
+        }
+        return History(res)
+    }
+
+    private fun getFullHistory(): List<History> {
+        val res = mutableListOf(getNext())
+        while (!res.last().isAllZeros()) {
+            res += res.last().getNext()
+        }
+        return res
+    }
+
+    fun isAllZeros(): Boolean = values.all { it == 0L }
+
+    fun extraPolate(): Long {
+        var prev: Long = 0L//values.last()
+        history.reversed().windowed(2, 1) {
+            prev += it.last().values.last()
+        }
+        return prev + values.last()
+    }
+}
