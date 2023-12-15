a650b7f864c849284c150df20b74fb13e4e7350f
Joost van Wollingen <joostvanwollingen@gmail.com>
2023-12-15 09:03:21 +0100

    Day 15 Test Part 2 OK

--- src/main/kotlin/day15.kt
+++ src/main/kotlin/day15.kt
@@ -1,10 +1,50 @@
 fun main() {
 //    val input = AocUtil.load("day15.test.input")
     val input = AocUtil.load("day15.test2.input")
+//    val input = AocUtil.load("day15.input")
+//    Day15PartOne(input)
 
+    val b: MutableMap<Long, MutableMap<String, Long>> = mutableMapOf()
+
+    val lenses = input.split(",").map { Regex("([a-z]+)([-=])+([0-9]?)").findAll(it) }.map { it.first() }.map { lens ->
+        Lens(lens.groupValues[1], lens.groupValues[2], lens.groupValues.getOrNull(3)?.toLongOrNull())
+    }
+
+    val h: Map<String, Long> = lenses.distinctBy { it.name }.map { it.name to hashString(it.name) }.toMap()
+
+    lenses.forEach { lens ->
+//        if (b[h[lens.name]] == null) return@forEach
+        if (lens.operation == "-") {
+            if (b[h[lens.name]]!!.containsKey(lens.name)) {
+                b[h[lens.name]!!] = b[h[lens.name]]!!.minus(lens.name).toMutableMap()
+            }
+        }
+        if (lens.operation == "=") {
+            if (b[h[lens.name]]?.containsKey(lens.name) == true) {
+                b[h[lens.name]]!!.replace(lens.name, lens.focalLength!!)
+            } else {
+                if (b[h[lens.name]!!].isNullOrEmpty()) b[h[lens.name]!!] = mutableMapOf()
+                b[h[lens.name]]?.put(lens.name, lens.focalLength!!)
+            }
+        }
+
+    }
+    println(b)
+    println(b.entries.flatMap { entry ->
+        val boxNumber: Long = entry.key + 1
+        entry.value.entries.mapIndexed { i, it -> boxNumber.times(it.value).times(i+1) }
+    }.sum())
+
+
+}
+
+
+private fun Day15PartOne(input: String) {
     val toBeHashed = input.split(",")
     println(toBeHashed.sumOf { hashString(it) })
 }
+
+data class Lens(val name: String, val operation: String, val focalLength: Long?)
 
 private fun hashString(input: String): Long =
     input.toCharArray().fold(0L) { acc, char -> (acc + char.code).times(17).rem(256) }
