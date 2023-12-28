3b1533d00a28b729177cfbde7ffe8490e7c5311c
Joost van Wollingen <joostvanwollingen@gmail.com>
2023-12-28 21:09:49 +0100

    2015 Day 15 & Day 16

---
+++ src/main/kotlin/nl/vanwollingen/aoc/aoc2015/Day15.kt
@@ -0,0 +1,83 @@
+package nl.vanwollingen.aoc.aoc2015
+
+import nl.vanwollingen.aoc.util.Puzzle
+import kotlin.math.max
+
+fun main() {
+    val d15 = Day15(2015, 15)
+    d15.solvePart1()
+    d15.solvePart2()
+}
+
+class Day15(year: Int, day: Int) : Puzzle(year, day) {
+
+    private val ingredients = parseInput()
+    private val sugar = ingredients.first { it.name == "Sugar" }
+    private val candy = ingredients.first { it.name == "Candy" }
+    private val chocolate = ingredients.first { it.name == "Chocolate" }
+    private val sprinkles = ingredients.first { it.name == "Sprinkles" }
+    override fun solvePart1() {
+        var highest = 0L
+        for (i in 100 downTo 0) {
+            for (j in 100 downTo 0) {
+                for (k in 100 downTo 0) {
+                    for (l in 100 downTo 0) {
+                        if (i + j + k + l == 100) {
+                            val score = getScore(sugar to i, candy to j, chocolate to k, sprinkles to l)
+                            highest = if (highest > score) highest else score
+                        }
+                    }
+                }
+            }
+        }
+        println(highest)
+    }
+
+    override fun solvePart2() {
+        var highest = 0L
+        for (i in 100 downTo 0) {
+            for (j in 100 downTo 0) {
+                for (k in 100 downTo 0) {
+                    for (l in 100 downTo 0) {
+                        if (i + j + k + l == 100 && getCalories(sugar to i, candy to j, chocolate to k, sprinkles to l) == 500L) {
+                            val score = getScore(sugar to i, candy to j, chocolate to k, sprinkles to l)
+                            highest = if (highest > score) highest else score
+                        }
+                    }
+                }
+            }
+        }
+        println(highest)
+    }
+
+    private fun getScore(vararg ingredients: Pair<Ingredient, Int>): Long {
+        val capacity = max(0, ingredients.sumOf { it.first.capacity.toLong() * it.second })
+        val durability = max(0, ingredients.sumOf { it.first.durability.toLong() * it.second })
+        val flavor = max(0, ingredients.sumOf { it.first.flavor.toLong() * it.second })
+        val texture = max(0, ingredients.sumOf { it.first.texture.toLong() * it.second })
+
+        return capacity * durability * flavor * texture
+    }
+
+    private fun getCalories(vararg ingredients: Pair<Ingredient, Int>): Long = ingredients.sumOf { it.first.calories.toLong() * it.second }
+
+    override fun parseInput(): List<Ingredient> = input.lines().map { line ->
+        Ingredient.fromString(line)
+    }
+
+    data class Ingredient(
+            val name: String,
+            val capacity: Int,
+            val durability: Int,
+            val flavor: Int,
+            val texture: Int,
+            val calories: Int,
+    ) {
+        companion object {
+            fun fromString(line: String): Ingredient {
+                val properties = Regex("(.*): capacity (-?\\d+), durability (-?\\d+), flavor (-?\\d+), texture (-?\\d+), calories (-?\\d+)").findAll(line).toList().first().groupValues
+                return Ingredient(properties[1], properties[2].toInt(), properties[3].toInt(), properties[4].toInt(), properties[5].toInt(), properties[6].toInt())
+            }
+        }
+    }
+}
---
+++ src/main/kotlin/nl/vanwollingen/aoc/aoc2015/Day16.kt
@@ -0,0 +1,85 @@
+package nl.vanwollingen.aoc.aoc2015
+
+import nl.vanwollingen.aoc.util.Puzzle
+
+fun main() {
+    val d16 = Day16(2015, 16)
+    d16.solvePart1()
+    d16.solvePart2()
+}
+
+class Day16(year: Int, day: Int) : Puzzle(year, day) {
+
+    private val aunties = parseInput()
+    private val children = 3
+    private val cats = 7
+    private val samoyeds = 2
+    private val pomeranians = 3
+    private val akitas = 0
+    private val vizslas = 0
+    private val goldfish = 5
+    private val trees = 3
+    private val cars = 2
+    private val perfumes = 1
+
+    override fun solvePart1() {
+        println(aunties.first { auntie ->
+            (auntie.children == -1 || auntie.children == children) && (auntie.cats == -1 || auntie.cats == cats) && (auntie.samoyeds == -1 || auntie.samoyeds == samoyeds) && (auntie.pomeranians == -1 || auntie.pomeranians == pomeranians) && (auntie.akitas == -1 || auntie.akitas == akitas) && (auntie.vizslas == -1 || auntie.vizslas == vizslas) && (auntie.goldfish == -1 || auntie.goldfish == goldfish) && (auntie.trees == -1 || auntie.trees == trees) && (auntie.cars == -1 || auntie.cars == cars) && (auntie.perfumes == -1 || auntie.perfumes == perfumes)
+        })
+    }
+
+    override fun solvePart2() {
+        println(aunties.first { auntie ->
+            (auntie.children == -1 || auntie.children == children) && (auntie.cats == -1 || auntie.cats > cats) && (auntie.samoyeds == -1 || auntie.samoyeds == samoyeds) && (auntie.pomeranians == -1 || auntie.pomeranians < pomeranians) && (auntie.akitas == -1 || auntie.akitas == akitas) && (auntie.vizslas == -1 || auntie.vizslas == vizslas) && (auntie.goldfish == -1 || auntie.goldfish < goldfish) && (auntie.trees == -1 || auntie.trees > trees) && (auntie.cars == -1 || auntie.cars == cars) && (auntie.perfumes == -1 || auntie.perfumes == perfumes)
+        })
+    }
+
+    override fun parseInput(): List<AuntSue> = input.lines().map { line -> AuntSue.fromString(line) }
+
+    data class AuntSue(
+            val number: Int,
+            val children: Int,
+            val cats: Int,
+            val samoyeds: Int,
+            val pomeranians: Int,
+            val akitas: Int,
+            val vizslas: Int,
+            val goldfish: Int,
+            val trees: Int,
+            val cars: Int,
+            val perfumes: Int,
+    ) {
+        companion object {
+            fun fromString(line: String): AuntSue {
+                val sueNumber = getFromRegex("Sue (\\d+):", line)
+                val children = getFromRegex("children: (\\d+)", line)
+                val cars = getFromRegex("cars: (\\d+)", line)
+                val vizslas = getFromRegex("vizslas: (\\d+)", line)
+                val goldfish = getFromRegex("goldfish: (\\d+)", line)
+                val akitas = getFromRegex("akitas: (\\d+)", line)
+                val samoyeds = getFromRegex("samoyeds: (\\d+)", line)
+                val perfumes = getFromRegex("perfumes: (\\d+)", line)
+                val trees = getFromRegex("trees: (\\d+)", line)
+                val pomeranians = getFromRegex("pomeranians: (\\d+)", line)
+                val cats = getFromRegex("cats: (\\d+)", line)
+
+                return AuntSue(
+                        number = sueNumber,
+                        children = children,
+                        cars = cars,
+                        vizslas = vizslas,
+                        goldfish = goldfish,
+                        akitas = akitas,
+                        samoyeds = samoyeds,
+                        perfumes = perfumes,
+                        trees = trees,
+                        pomeranians = pomeranians,
+                        cats = cats,
+                )
+            }
+
+            private fun getFromRegex(regex: String, line: String) = (Regex(regex).find(line).takeIf { it != null }?.let { it.groups[1]?.value?.toInt() }
+                    ?: -1)
+        }
+    }
+}
