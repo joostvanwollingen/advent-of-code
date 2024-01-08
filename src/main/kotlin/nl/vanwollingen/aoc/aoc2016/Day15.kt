d7dee68f7b70a0ecf1d5b030b68f0a037e19b0a4
Joost van Wollingen <joostvanwollingen@gmail.com>
2024-01-08 21:06:21 +0100

    2016 Day 15

---
+++ src/main/kotlin/nl/vanwollingen/aoc/aoc2016/Day15.kt
@@ -0,0 +1,61 @@
+package nl.vanwollingen.aoc.aoc2016
+
+import nl.vanwollingen.aoc.util.Puzzle
+
+fun main() {
+    val d15 = Day15()
+    d15.solvePart1()
+    d15.solvePart2()
+}
+
+class Day15(output: Boolean = false) : Puzzle(output) {
+
+    private val discs = parseInput()
+    override fun parseInput(): List<Disc> {
+        val parseLineRegex = Regex("Disc #(\\d) has (\\d+) positions; at time=0, it is at position (\\d+).")
+        return input.lines().mapIndexed { i, line ->
+            val parsedLine = parseLineRegex.findAll(line).toList().first().groupValues
+            Disc(parsedLine[1].toInt(), parsedLine[2].toInt(), parsedLine[3].toInt())
+        }
+    }
+
+    override fun solvePart1() {
+        log(synchronizeDiscs(discs))
+    }
+
+    override fun solvePart2() {
+        val extraDisc = parseInput().plus(Disc(7, 11, 0))
+        log(synchronizeDiscs(extraDisc))
+    }
+
+    private fun synchronizeDiscs(discs: List<Disc>): Int {
+        var t = 0
+        while (true) {
+            t++
+            discs.forEach { disc ->
+                disc.tick()
+            }
+            if (discs.all { disc -> disc.positionAfterTicks(disc.id) == 0 }) {
+                return t
+            }
+        }
+    }
+
+    data class Disc(val id: Int, val numberOfPositions: Int, val startPosition: Int, var currentPosition: Int = startPosition) {
+        fun positionAfterTicks(numberOfTicks: Int): Int {
+            var ticks = numberOfTicks
+            var newPosition = currentPosition
+            while (ticks > 0) {
+                newPosition += 1
+                if (newPosition == numberOfPositions) newPosition = 0
+                ticks--
+            }
+            return newPosition
+        }
+
+        fun tick(): Int {
+            currentPosition = positionAfterTicks(1)
+            return currentPosition
+        }
+    }
+}
