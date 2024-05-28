2af5a506f65b76b6dbbb70370b3d5376528941ef
Joost van Wollingen <joostvanwollingen@gmail.com>
2024-05-28 21:32:08 +0200

    2017 Day 12 Part 2

--- src/main/kotlin/nl/vanwollingen/aoc/aoc2017/Day12.kt
+++ src/main/kotlin/nl/vanwollingen/aoc/aoc2017/Day12.kt
@@ -6,7 +6,9 @@ import java.util.Queue
 
 fun main() {
     val d12 = Day12()
+    d12.parseInput()
     d12.solvePart1()
+    d12.solvePart2()
 }
 
 class Day12(output: Boolean = false) : Puzzle(output) {
@@ -14,26 +16,34 @@ class Day12(output: Boolean = false) : Puzzle(output) {
     private val pipesMap: MutableMap<Int, Set<Int>> = mutableMapOf()
     private val startPipe = 0
 
-    override fun parseInput() = input.lines().map { l -> Pipe.fromString(l) }
-        .forEach { p ->
-            val k = pipesMap.getOrPut(p.from) { p.to }
-            if (k != p.to) {
-                pipesMap[p.from] = k.plus(p.to)
-            }
-
-            p.to.forEach { t ->
-                val d = pipesMap.getOrPut(t) { setOf(p.from) }
-                if (d != setOf(p.from)) {
-                    pipesMap[t] = d.plus(p.from)
-                }
-            }
+    override fun parseInput() = input.lines().map { l -> Pipe.fromString(l) }.forEach { p ->
+        val k = pipesMap.getOrPut(p.from) { p.to }
+        if (k != p.to) {
+            pipesMap[p.from] = k.plus(p.to)
         }
 
+        p.to.forEach { t ->
+            val d = pipesMap.getOrPut(t) { setOf(p.from) }
+            if (d != setOf(p.from)) {
+                pipesMap[t] = d.plus(p.from)
+            }
+        }
+    }
+
     override fun part1() {
-        parseInput()
-        val visited: MutableSet<Int> = mutableSetOf(startPipe)
+        val visited = getGroupForPipe(startPipe, pipesMap)
+        log(visited.size)
+    }
+
+    override fun part2() {
+        val allGroups = pipesMap.map { getGroupForPipe(it.key, pipesMap) }.distinct()
+        log(allGroups.size)
+    }
+
+    private fun getGroupForPipe(pipe: Int, pipesMap: MutableMap<Int, Set<Int>>): MutableSet<Int> {
+        val visited: MutableSet<Int> = mutableSetOf(pipe)
         val q: Queue<Int> = LinkedList()
-        q.add(startPipe)
+        q.add(pipe)
 
         while (q.isNotEmpty()) {
             val v = q.poll()
@@ -43,12 +53,7 @@ class Day12(output: Boolean = false) : Puzzle(output) {
 
             q.addAll(newPipes)
         }
-
-        log(visited.size)
-    }
-
-    override fun part2() {
-        TODO("Not yet implemented")
+        return visited
     }
 
     data class Pipe(val from: Int, val to: MutableSet<Int>) {
