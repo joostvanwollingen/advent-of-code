8d23a161959fd502aadc8c5722d8c05941391eb6
Joost van Wollingen <joostvanwollingen@gmail.com>
2024-12-23 23:44:16 +0100

    2024 Day 23 Part 1 & 2

---
+++ src/main/kotlin/nl/vanwollingen/aoc/aoc2024/Day23.kt
@@ -0,0 +1,83 @@
+package nl.vanwollingen.aoc.aoc2024
+
+import nl.vanwollingen.aoc.util.Puzzle
+import java.util.LinkedList
+
+fun main() = Day23.solve()
+
+object Day23 : Puzzle(exampleInput = false) {
+
+    private val connections = parseInput()
+
+    override fun parseInput(): MutableMap<String, MutableSet<String>> {
+        val connectionMap = mutableMapOf<String, MutableSet<String>>()
+
+        extractPairs().forEach { (from, to) ->
+            val fromConnections = connectionMap.getOrDefault(from, mutableSetOf())
+            fromConnections.add(to)
+            connectionMap[from] = fromConnections.sorted().toMutableSet()
+
+            val toConnections = connectionMap.getOrDefault(to, mutableSetOf())
+            toConnections.add(from)
+            connectionMap[to] = toConnections.sorted().toMutableSet()
+        }
+        return connectionMap
+    }
+
+    private fun extractPairs(): List<Pair<String, String>> {
+        val connections = input.lines().map {
+            val (from, to) = it.split("-")
+            from to to
+        }
+        return connections
+    }
+
+    override fun part1(): Any {
+        val trips: MutableSet<Triple<String, String, String>> = mutableSetOf()
+        val withT = connections.keys.filter { it.startsWith("t") }
+
+        withT.forEach { computer ->
+            val computerConnections = connections[computer] ?: throw Error("fail")
+            computerConnections.forEach { conn ->
+                val newConnections = connections[conn] ?: throw Error("fail")
+                val sharedConnections = newConnections.intersect(computerConnections)
+                sharedConnections.forEach { s ->
+                    val (one, two, three) = listOf(computer, conn, s).sorted()
+                    trips.add(Triple(one, two, three))
+                }
+            }
+        }
+        return trips.size
+    }
+
+
+    override fun part2(): Any {
+        val networks = mutableSetOf<Set<String>>()
+
+        connections.keys.forEach { computer ->
+            val network = mutableSetOf<String>()
+            network.add(computer)
+            val connectedComputers = LinkedList<String>().apply { addAll(connections[computer]!!) }
+            while (connectedComputers.isNotEmpty()) {
+                val otherComputer = connectedComputers.poll()
+                if (isConnectedToAll(network + otherComputer)) network.add(otherComputer)
+            }
+            networks.add(network)
+        }
+
+        return networks
+            .maxBy { it.size }
+            .sorted()
+            .joinToString(",")
+    }
+
+    private fun isConnectedToAll(set: Set<String>): Boolean {
+        set.forEach { c ->
+            set.forEach inner@{ d ->
+                if (d == c) return@inner
+                if (!connections[c]!!.contains(d)) return false
+            }
+        }
+        return true
+    }
+}
