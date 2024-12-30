4b7b064ee67f3962c6f9c5301dc74bf7e86a108b
Joost van Wollingen <joostvanwollingen@gmail.com>
2024-12-30 23:56:41 +0100

    Infi 2024 puzzle

--- src/main/kotlin/nl/vanwollingen/aoc/infi/aoc2024/Y2024Infi.kt
+++ src/main/kotlin/nl/vanwollingen/aoc/infi/aoc2024/Y2024Infi.kt
@@ -2,34 +2,91 @@ package nl.vanwollingen.aoc.infi.aoc2024
 
 import nl.vanwollingen.aoc.util.Puzzle
 import java.util.LinkedList
+import kotlin.random.Random
 
 fun main() = Y2024Infi().solve()
 
 //WC8N85QQ8649
 class Y2024Infi : Puzzle(exampleInput = false) {
-    override fun part1(): Int {
-        val instructions: Map<Int, Pair<String, String?>> = getInstructions()
+    private val instructions: Map<Int, Pair<String, String?>> = getInstructions()
 
-        var sum = 0
-        val map: MutableList<Pair<Triple<Int, Int, Int>, Int>> = mutableListOf()
+    override fun part1() = buildSnowMap().values.sum()
+
+    override fun part2(): Int {
+        val snowMap: Map<Triple<Int, Int, Int>, Int> = buildSnowMap()
+
+        val clouds = mutableListOf<Int>()
+        val visited: MutableSet<Triple<Int, Int, Int>> = mutableSetOf()
+
         for (x in 0..29) {
             for (y in 0..29) {
                 for (z in 0..29) {
-//                    map.add(Triple(x, y, z) to instructions(x, y, z, operations))
-                    sum += calculateSnowContents(x, y, z, instructions)
+                    val current = Triple(x, y, z)
+                    if (snowMap[current] != null && !visited.contains(current)) {
+                        clouds += findCloud(current, visited, snowMap)
+                    }
                 }
             }
         }
-//        return map.sumOf { it.second }
-        return sum
+        return clouds.size
     }
 
+    private fun buildSnowMap(): Map<Triple<Int, Int, Int>, Int> {
+        val snowMap: MutableMap<Triple<Int, Int, Int>, Int> = mutableMapOf()
 
-    override fun part2(): Any {
-        TODO("Not yet implemented")
+        for (x in 0..29) {
+            for (y in 0..29) {
+                for (z in 0..29) {
+                    calculateSnowContents(x, y, z, instructions).let { snow ->
+                        if (snow > 0) {
+                            snowMap[Triple(x, y, z)] = snow
+                        }
+                    }
+                }
+            }
+        }
+        return snowMap
     }
 
-    private fun calculateSnowContents(x: Int, y: Int, z: Int, instructions: Map<Int, Pair<String, String?>>): Int {
+    private fun findCloud(
+        current: Triple<Int, Int, Int>,
+        visited: MutableSet<Triple<Int, Int, Int>>,
+        snowMap: Map<Triple<Int, Int, Int>, Int>
+    ): Int {
+        val cloudId = Random.nextInt()
+        val cloudQueue = LinkedList<Triple<Int, Int, Int>>()
+        cloudQueue.add(current)
+
+        while (cloudQueue.isNotEmpty()) {
+            val next = cloudQueue.poll()
+            val neighbors = getNeighbourClouds(next, snowMap)
+            visited.add(next)
+            cloudQueue.addAll(neighbors - visited)
+            visited.addAll(neighbors)
+        }
+        return cloudId
+    }
+
+    private fun getNeighbourClouds(
+        next: Triple<Int, Int, Int>,
+        map: Map<Triple<Int, Int, Int>, Int>,
+    ): List<Triple<Int, Int, Int>> {
+        val (x, y, z) = next
+
+        val west = Triple(x - 1, y, z)
+        val east = Triple(x + 1, y, z)
+        val north = Triple(x, y + 1, z)
+        val south = Triple(x, y - 1, z)
+        val up = Triple(x, y, z + 1)
+        val down = Triple(x, y, z - 1)
+
+        return listOf(west, east, up, down, north, south).mapNotNull { if (map[it] != null) it else null }
+    }
+
+    private fun calculateSnowContents(
+        x: Int, y: Int, z: Int,
+        instructions: Map<Int, Pair<String, String?>>,
+    ): Int {
         var counter = 0
         val stack = LinkedList<Int>()
 
