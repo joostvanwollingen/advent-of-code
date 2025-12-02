98cd1936ea6d070cc4d073c9cb2cf23ce3b3dbac
Joost van Wollingen <joostvanwollingen@gmail.com>
2025-12-02 20:18:08 +0100

    2025 Day 2 Cleanup

--- src/main/kotlin/nl/vanwollingen/aoc/aoc2025/Day02.kt
+++ src/main/kotlin/nl/vanwollingen/aoc/aoc2025/Day02.kt
@@ -6,49 +6,35 @@ fun main() = Day02.solve()
 
 object Day02 : Puzzle(true, false) {
 
-    private val ranges = parseInput()
-    override fun parseInput() = input.split(",").map { range ->
+    private val productIds = parseInput()
+
+    override fun parseInput() = input.split(",").flatMap { range ->
         val (start, end) = range.split("-").map { it.toLong() }
         start..end
     }
 
-    override fun part1(): Any {
-        return ranges.sumOf { range ->
-            range.sumOf { num ->
-                val string = num.toString()
-                if (string.length % 2 != 0) 0
-                else {
-                    val first = string.subSequence(0, string.length / 2)
-                    val second = string.subSequence(string.length / 2, string.length)
-                    if (first == second) num else 0
-                }
-            }
-        }
+    override fun part1() = productIds.sumOf { productId -> isRepeatingHalf(productId) }
+    override fun part2() = productIds.sumOf { productId -> hasRepeatedPattern(productId) }
+
+    private fun isRepeatingHalf(number: Long): Long {
+        val string = number.toString()
+        if (string.length % 2 != 0) return 0
+        val first = string.subSequence(0, string.length / 2)
+        val second = string.subSequence(string.length / 2, string.length)
+        return if (first == second) number else 0
     }
 
-    override fun part2(): Any {
-        return ranges.sumOf { range ->
-            range.sumOf { num ->
-                val string = num.toString()
-                val pattern = findPattern(string)
-                if (pattern == null) 0L
-                else {
-                    num
-                }
-            }
-        }
-    }
-
-    private fun findPattern(string: String): String? {
+    private fun hasRepeatedPattern(number: Long): Long {
+        val string = number.toString()
         var patternToTest = string.substring(0, string.length / 2)
 
         while (patternToTest.isNotEmpty()) {
             val chunks = string.chunked(patternToTest.length)
             if (chunks.all { it == patternToTest }) {
-                return patternToTest
+                return string.toLong()
             }
             patternToTest = patternToTest.dropLast(1)
         }
-        return null
+        return 0L
     }
 }
